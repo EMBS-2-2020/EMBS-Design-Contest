c74ee2e86c5be140d25e7a2028086b377bf73021
Harry Burge <40576217+HarryBurge@users.noreply.github.com>
2020-11-26 12:27:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def main():
 
 def gen_main():
     # Consts
-    pop_size = 1000
+    pop_size = 10
     mutation_rate = 0.01
     meshX = 3
     meshY = 3
+    runs = 100
 
     # Generate population
     def create_random_mapping():
@@ -28,17 +29,61 @@ def gen_main():
 
     population_mappings = [create_random_mapping() for i in range(pop_size)]
 
-    # Sim population collect costs and inf if fail
+    for k in range(runs):
+        # Sim population collect costs and inf if fail
+        # costs = []
 
-    # Invert costs need to find range
+        # for mapping in population_mappings:
+        #     try:
+        #         sim = Simulation.Simulation(meshX, meshY, 1, 1, mapping)
+        #         costs.append(sim.get_cost_mark())
+        #     except Exception:
+        #         costs.append(1000)
 
-    # Breed population
 
-    # Apply mutations
+        # Invert costs need to find range
+        # TODO: Can't do yet until sim can run and get a good guage on the range
+        inv_costs = [1]*10
+
+        inv_costs = np.asarray(inv_costs)
+
+
+        # Breed population
+        distribution = inv_costs/sum(inv_costs)
+
+        choices1 = np.random.choice(np.arange(pop_size), pop_size, p=distribution)
+        choices2 = np.random.choice(np.arange(pop_size), pop_size, p=distribution)
+
+        newpop = []
+
+        for i in range(pop_size):
+            mapping1 = population_mappings[choices1[i]]
+            mapping2 = population_mappings[choices2[i]]
+
+            newmapping = []
+
+            for (tsk1, x1, y1,),(tsk2, x2, y2) in zip(mapping1, mapping2):
+                
+                if np.random.random() > mutation_rate:
+
+                    # No mutation
+                    if np.random.randint(1, 11, 1) > 5:
+                        newmapping.append((tsk1, x1, y1))
+                    else:
+                        newmapping.append((tsk2, x2, y2))
+
+                else:
+
+                    # Mutation
+                    newmapping.append((tsk1, np.random.randint(0, meshX), np.random.randint(0, meshY)))
+
+            newpop.append(newmapping)
+
+        population_mappings = newpop
 
     # Repeat
     return None
 
 
 if __name__ == '__main__':
-    main()
+    gen_main()
